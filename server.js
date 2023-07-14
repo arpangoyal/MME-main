3b91f2685b914328d580f3fc964e23cc2350a3db
arpangoyal <arpangoyal2002@gmail.com>
2023-07-14 17:44:45 +0530

    q

--- server.js
+++ server.js
@@ -19,7 +19,7 @@
   app.use(express.json());
   app.use(cors());
   app.use(express.static(path.join(__dirname ,'public')));
-  const port = process.env.PORT || 5000;
+  const port = 5000;
 
   
   // mongoose.connect('mongodb://localhost:27017/reactdata', { useNewUrlParser: true });
